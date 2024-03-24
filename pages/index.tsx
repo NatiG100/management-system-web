8ac96cf75bb315d23f7d6f248363d6971c41520f
NatiG100 <natnaelgashu2022@gmail.com>
2024-03-24 17:21:53 +0300

    feature: complete dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,63 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { Button } from "@mantine/core";
-import { useDispatch } from "react-redux";
-import { logoutUserAction } from "@/context/user-slice/slice";
-import { Navbar } from "@/components/Sidebar";
+import { Button, Divider, Flex, Group, Paper, Stack, Table, Title } from "@mantine/core";
+import {  useSelector } from "react-redux";
+import { StateType } from "@/context/rootReducer";
+import { useState } from "react";
+import Link from "next/link";
+import {IconUser} from "@tabler/icons-react"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <p>DAshboard</p>
-    </div>
-  )
+  const [hilightedDepartment,setHilightedDepartment] = useState<string>('');
+    function hilightDepartment(depId:string){
+        setHilightedDepartment(depId);
+        setTimeout(()=>{setHilightedDepartment('')},4000);
+    }
+  const {data,errors,isLoading} = useSelector((state:StateType)=>state.department.department);
+    return(
+      <Stack>
+        <Paper withBorder className="w-max" bg={"green"} c="var(--mantine-color-white)" shadow="sm" px={20} py={10}>
+          <Flex align={"center"} gap={15}><IconUser/><Title size={25}>{data?.length} Departments</Title></Flex>
+        </Paper>
+        <Divider color="var(--mantine-primary-color-2)" />
+        <Title >Department List</Title>
+        <Table>
+            <Table.Thead>
+                <Table.Tr>
+                    <Table.Th>Id</Table.Th>
+                    <Table.Th>Name</Table.Th>
+                    <Table.Th>Description</Table.Th>
+                    <Table.Th>Parent Id</Table.Th>
+                </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+                {
+                    data?.length===0?
+                    <Table.Td>No Department</Table.Td>:
+                    data?.map((department)=>(<Table.Tr id={department.id} key={department.id} className={`${department.id===hilightedDepartment?'text-blue-500 animate-pulse':"animate-none"}`}>
+                        <Table.Td>{department.id}</Table.Td>
+                        <Table.Td>{department.name}</Table.Td>
+                        <Table.Td>{department.description}</Table.Td>
+                        <Table.Td>{
+                            department.relationsAsAChild[0]?.parentId?
+                            <Link
+                                className="underline text-green-700" 
+                                href={`#${department.relationsAsAChild[0].parentId}`} 
+                                onClick={()=>hilightDepartment(department.relationsAsAChild[0].parentId)}
+                            >{department.relationsAsAChild[0]?.parentId}</Link>:
+                            "-"}</Table.Td>
+                    </Table.Tr>))
+                }
+            </Table.Tbody>
+        </Table>
+        <Divider color="var(--mantine-primary-color-2)" />
+        <Title>Actions</Title>
+        <Group>
+          <Button>Manage Department</Button>
+          <Button>View Profile</Button>
+        </Group>
+      </Stack>
+    )
 }
